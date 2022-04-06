d913ad2531f4296ee3509bd8f06139bb1132b45e
Your Name <you@example.com>
2022-04-06 22:38:54 +0530

    my changes

---
+++ app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const bodyParse=require('body-parser');
+const mysql=require('mysql');
+require('dotenv').config();
+const app=express();
+const port=process.env.PORT||5000;
+const exphbs=require('express-handlebars');
+app.use(bodyParse.urlencoded({extended: false}));
+app.use(bodyParse.json());
+app.use(express.static('public'));
+app.engine('hbs',exphbs.engine({
+    extname:'.hbs',
+    defaultLayout: false
+
+
+}));
+app.set('view engine','hbs');
+app.get('',(req,res)=>{
+    res.render('index');
+
+});
+
+
+
+
+
+app.listen(port,()=> console.log('server started succesfulluy on port',port));
+
